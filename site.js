fc313a010eccb95f7eef2462531730c1d96b9e0c
DanielB566 <danib7382@gmail.com>
2024-12-12 11:07:54 -0500

    Added the additonal variables to the vue app.

--- site.js
+++ site.js
@@ -34,6 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Dan's Top 8 Movies",
+            owner: "Daniel Bootnik",
+            github: "https://github.com/DanielB566/NJIT-Project-2"
             
       }
     },
